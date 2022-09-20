0d7d94c253c8c26d165caeaf82d7c6915d609e19
sanjeev <sanjeevsingh.77d@gmail.com>
2022-09-21 01:22:01 +0530

    After flipping 21 times win status

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,21 +2,40 @@
 
 Head=1;
 tails=0;
+tie=2;
 HeadCount=0;
 TailCount=0;
+tieCount=0;
 
-for (( i = 1; i <= 20; i++ ))
+for (( i = 1; i > 0; i++ ))
 do
-	checkRandom=$((RANDOM%2))
+	checkRandom=$((RANDOM%3))
 
 	if [ $checkRandom -eq 1 ]
 	then
 		HeadCount=$(( $HeadCount + 1 ))
-	else
+		if [ $HeadCount -eq 21 ]
+		then
+			result=$(( HeadCount - TailCounter ))
+			echo "Heads win by $result diff."
+			exit
+		fi
+	elif [ $checkRandom -eq 0 ]
+	then
 		TailCount=$(( $TailCount + 1 ))
+		if [ $TailCount -eq 21 ]
+		then
+			result=$(( TailCount - HeadCounter ))
+			echo "Tails win by $result diff."
+			exit
+		fi
+	else
+		tieCount=$(( $tieCount + 1 ))
+		if [ $tieCount -eq 21 ]
+		then
+			echo "TIE"
+			exit
+		fi
 	fi
 
 done
-
-echo "Heads is come $HeadCount";
-echo "isTails is come $TailCount";
